e20bc76955b1807be7dd20fc03bee92f886565da
leshacat <43627985+leshacat@users.noreply.github.com>
2020-01-25 19:54:32 -0500

    build

---
+++ build/CMakeFiles/webchain-miner.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kthx/MINERS/webchain-miner/src/crypto/Lyra2.c" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/crypto/Lyra2.c.o"
+  "/home/kthx/MINERS/webchain-miner/src/crypto/Sponge.c" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/crypto/Sponge.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_AEON"
+  "XMRIG_NO_IPBC"
+  "XMRIG_NO_SUMO"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kthx/MINERS/webchain-miner/src/App.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/App.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/App_unix.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/App_unix.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/Cpu.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/Cpu.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/Cpu_unix.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/Cpu_unix.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/Mem.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/Mem.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/Mem_unix.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/Mem_unix.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/Summary.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/Summary.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/api/Api.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/api/Api.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/api/ApiRouter.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/api/ApiRouter.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/api/NetworkState.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/api/NetworkState.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/Console.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/Console.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/Platform.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/Platform.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/Platform_unix.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/Platform_unix.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/api/HttpRequest.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/api/HttpRequest.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/api/Httpd.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/api/Httpd.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/config/CommonConfig.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/config/CommonConfig.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/config/ConfigLoader.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/config/ConfigLoader.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/config/ConfigWatcher.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/config/ConfigWatcher.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/crypto/Algorithm.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/crypto/Algorithm.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/crypto/keccak.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/crypto/keccak.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/log/ConsoleLog.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/log/ConsoleLog.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/log/FileLog.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/log/FileLog.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/log/Log.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/log/Log.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/log/SysLog.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/log/SysLog.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/Client.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/Client.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/Job.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/Job.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/Pool.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/Pool.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/SubmitResult.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/SubmitResult.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/strategies/FailoverStrategy.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/strategies/FailoverStrategy.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/common/net/strategies/SinglePoolStrategy.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/common/net/strategies/SinglePoolStrategy.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/core/Config.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/core/Config.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/core/Controller.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/core/Controller.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/net/Network.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/net/Network.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/net/strategies/DonateStrategy.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/net/strategies/DonateStrategy.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/CpuThread.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/CpuThread.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/Handle.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/Handle.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/Hashrate.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/Hashrate.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/MultiWorker.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/MultiWorker.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/Worker.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/Worker.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/workers/Workers.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/workers/Workers.cpp.o"
+  "/home/kthx/MINERS/webchain-miner/src/xmrig.cpp" "/home/kthx/MINERS/webchain-miner/build/CMakeFiles/webchain-miner.dir/src/xmrig.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_SYSLOG_H"
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "XMRIG_NO_AEON"
+  "XMRIG_NO_IPBC"
+  "XMRIG_NO_SUMO"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "../src"
+  "../src/3rdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kthx/MINERS/webchain-miner/build/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
